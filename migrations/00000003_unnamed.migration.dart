2944d3582e301ebca3973009b1bbb6bac81d2822
Merfy <96495867+ArmAtrix123@users.noreply.github.com>
2023-02-28 14:17:08 +0300

    create: api

---
+++ migrations/00000003_unnamed.migration.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+import 'package:conduit_core/conduit_core.dart';   
+
+class Migration3 extends Migration { 
+  @override
+  Future upgrade() async {
+   		database.createTable(SchemaTable("_Logs", [SchemaColumn("id", ManagedPropertyType.bigInteger, isPrimaryKey: true, autoincrement: true, isIndexed: false, isNullable: false, isUnique: false),SchemaColumn("operationDate", ManagedPropertyType.datetime, isPrimaryKey: false, autoincrement: false, isIndexed: true, isNullable: false, isUnique: false),SchemaColumn("description", ManagedPropertyType.string, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: false, isUnique: false)]));
+		database.addColumn("_Logs", SchemaColumn.relationship("owner", ManagedPropertyType.bigInteger, relatedTableName: "_User", relatedColumnName: "id", rule: DeleteRule.nullify, isNullable: true, isUnique: false));
+		database.deleteTable("_Logger");
+  }
+  
+  @override
+  Future downgrade() async {}
+  
+  @override
+  Future seed() async {}
+}
+    
